a66e2fa8799c00906bf9083561a99c68278fe827
Brenda Ferrufino <ferrufinob@gmail.com>
2021-06-07 15:40:18 -0400

    able to display and upload images

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,8 @@
 class Pin < ApplicationRecord
+
   has_one_attached :image
   belongs_to :user
   belongs_to :board
 
-  validates :title, :description, :image, :site_url, presence: true
+  validates :title, :description, :site_url, presence: true
 end
--- app/serializers/pin_serializer.rb
+++ app/serializers/pin_serializer.rb
@@ -1,11 +1,10 @@
 class PinSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
   attributes :id, :title, :description, :image, :site_url, :user_id, :board_id
 
   def image
     if object.image.attached?
-      {
-        url: rails_blob_url(object.image),
-      }
+      rails_blob_url(object.image)
     end
   end
 end
