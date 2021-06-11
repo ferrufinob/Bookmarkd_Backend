2d5286998719d10c492c974113bdc3b6c6d7b726
Brenda Ferrufino <ferrufinob@gmail.com>
2021-06-10 21:33:36 -0400

    small typo

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,6 @@
 class Board < ApplicationRecord
   belongs_to :user
 
-  validates :name, uniqueness: { scope: :user, message: " already exists for this brand" }
+  validates :name, uniqueness: { scope: :user, message: " already exists for this board" }
   validates :name, presence: true
 end
