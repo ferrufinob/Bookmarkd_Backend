ffb5b6b9c20fbad9da66f5ce47ed00c5724d9367
Brenda Ferrufino <ferrufinob@gmail.com>
2021-09-11 15:27:55 -0400

    added dependent destroy to board

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,6 @@
 class Board < ApplicationRecord
   belongs_to :user
+  has_many :pins, dependent: :destroy
 
   validates :name, uniqueness: { scope: :user, message: " already exists for this board" }
   validates :name, presence: true
